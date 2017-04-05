83918ead15d190483ed5757da287d995aded5a2f
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-04-05 15:48:29 -0400

    Added BillsIndex branch

---
+++ App_Code/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Config
+/// </summary>
+public class Config
+{
+    public Config()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
