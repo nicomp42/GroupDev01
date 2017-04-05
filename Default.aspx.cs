6c3bd05680f9b4e93b331d040513ee05c6f830f5
John Tellep <tellepjc@mail.uc.edu>
2017-04-05 18:39:39 -0400

    Instantiated objects in the page load event in default.aspx

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -6,7 +6,30 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page {
-    protected void Page_Load(object sender, EventArgs e) {
+
+    static Group01 group01;
+    static Group02 group02;
+    static Group03 group03;
+    static Group04 group04;
+    static Group05 group05;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        instantiateObjects();
+    }
+
+    private void instantiateObjects()
+    {
+        group01 = new Group01();
+        group02 = new Group02();
+        group03 = new Group03();
+        group04 = new Group04();
+        group05 = new Group05();
+    }
+
+    private void Group01Method()
+    {
+        int productIDWithMostReturns = group01.GetProductIDWithTheMostReturns();
+
 
     }
 }
